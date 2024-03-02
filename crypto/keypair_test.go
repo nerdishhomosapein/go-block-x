f673a2ad429ea85da86379c414d7e280d7b71e94
kumar prateek <kumarprateek902@gmail.com>
2024-03-02 14:47:26 +0530

    Added tests

--- crypto/keypair_test.go
+++ crypto/keypair_test.go
@@ -6,15 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratePrivateKey(t *testing.T) {
+func TestKeyPairSignVerifySuccess(t *testing.T) {
 
 	privKey := GeneratePrivatekey()
 	pubKey := privKey.PublicKey()
-	// address := pubKey.Address()
+	msg := []byte("hello, world")
 
-	msg := []byte("hello, world!!!")
 	sig, err := privKey.Sign(msg)
+
 	assert.Nil(t, err)
 	assert.True(t, sig.Verify(pubKey, msg))
+}
 
+func TestKeyPairSignVerifyFail(t *testing.T) {
+
+	privKey := GeneratePrivatekey()
+	pubKey := privKey.PublicKey()
+	msg := []byte("hello, world")
+  otherMsg := []byte("different hellow world")
+
+	sig, err := privKey.Sign(msg)
+
+	assert.Nil(t, err)
+	otherPrivKey := GeneratePrivatekey()
+	otherPublicKey := otherPrivKey.PublicKey()
+
+	assert.False(t, sig.Verify(otherPublicKey, msg))
+  assert.False(t, sig.Verify(pubKey, otherMsg))
 }
